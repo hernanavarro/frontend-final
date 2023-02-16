fee4ce57e50c89ae74814cc77768b72987d58162
Hernan Navarro <navarro.hernan97@gmail.com>
2023-02-16 19:49:57 -0300

    Add functions and botons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import Button from '@mui/material/Button';
 import './App.css';
+import logo from './logo.svg';
+
+import { useState } from 'react';
 
 function App() {
+
+  const [contador, setContador] = useState(0);
+  //const [contador, setContador] = useState(0);
+  const add = async () => {
+    const res = await fetch('http://localhost:5050/add/2')
+    const obj = await res.json();
+    setContador(obj['value']);
+  }
+
+  const subtract = async () => {
+    const res = await fetch('http://localhost:5050/subtract/3')
+    const obj = await res.json();
+    setContador(obj['value']);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the final project of the subject "Software Engineering 3".
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <Button variant="contained"  onClick = {add}>Sumar</Button>
+          <Button variant="outlined" onClick = {subtract}>Restar</Button>
+        </div>      
       </header>
     </div>
   );
