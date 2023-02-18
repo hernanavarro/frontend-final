4e28d89d44dda1ab5d6a95f8274c6ed467b6f2bb
Hernan Navarro <navarro.hernan97@gmail.com>
2023-02-17 23:50:12 -0300

    Add test and docker in pipeline

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 
-describe('Operators module', () => {
+describe('App render', () => {
   test('Render Botons', () => {
     render(<App />);
 
